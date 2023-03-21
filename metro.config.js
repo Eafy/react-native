ca42e7aaed175cded3c4922bf67fd95d8f2368a4
eafy <lizhijian_21@163.com>
2023-03-21 10:01:04 +0800

    1、更新为0.63.2版本代码；2、修复Android可查找APK包中的图片资源；

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,17 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
  * @format
  */
 
-'use strict';
-
-const getPolyfills = require('./rn-get-polyfills');
-
-/**
- * This cli config is needed for development purposes, e.g. for running
- * integration tests during local development or on CI services.
- */
 module.exports = {
-  resolver: {
-    extraNodeModules: {
-      'react-native': __dirname,
-    },
-  },
-  serializer: {
-    getPolyfills,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
   },
 };
